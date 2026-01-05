5ec5b5e6f7feb280cf1b14e8152f9447184d303d
hasansirac <hasansirac.ozbeyler1@gmail.com>
2026-01-06 00:00:02 +0300

    Fix: Added validation to prevent negative menu item prices

--- src/MenuItem.java
+++ src/MenuItem.java
@@ -3,23 +3,25 @@ public class MenuItem {
     private double price;
     private String category;
 
-
+    // Fiyat kontrolü eklenmiş Constructor
     public MenuItem(String name, double price, String category) {
         this.name = name;
-        this.price = price;
+        if (price < 0) {
+            System.out.println("Warning: Price cannot be negative! Setting to 0.");
+            this.price = 0;
+        } else {
+            this.price = price;
+        }
         this.category = category;
     }
-
 
     public String getName() {
         return name;
     }
 
-
     public double getPrice() {
         return price;
     }
-
 
     @Override
     public String toString() {
