f3474acb2ef5d26d2bbc5262ffd6550e8adaf78a
hasansirac <hasansirac.ozbeyler1@gmail.com>
2026-01-05 21:31:17 +0300

    Feat: Implemented Order class with logic

---
+++ src/Order.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Order implements Orderable {
+    private int orderId;
+    private List<MenuItem> items;
+    private double totalAmount;
+    private Payment paymentMethod;
+
+    public Order(int orderId) {
+        this.orderId = orderId;
+        this.items = new ArrayList<>();
+        this.totalAmount = 0.0;
+    }
+
+    public void addItem(MenuItem item) {
+        items.add(item);
+    }
+
+    public double calculateTotal() {
+        totalAmount = 0.0;
+        for (MenuItem item : items) {
+            totalAmount += item.getPrice();
+        }
+        return totalAmount;
+    }
+
+    @Override
+    public void placeOrder() {
+        System.out.println("Order placed successfully with ID: " + orderId);
+    }
+
+    public void setPaymentMethod(Payment pm) {
+        this.paymentMethod = pm;
+    }
+}
